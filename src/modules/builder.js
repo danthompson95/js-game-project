f506dde542abe3810dcaab3d5308b7483b7919eb
Daniel Thompson <florestina95@gmail.com>
2020-04-16 16:46:57 +0200

    Added prettier and applied formatting

--- src/modules/builder.js
+++ src/modules/builder.js
@@ -1,53 +1,52 @@
 export function resourcePanel(foodImg, stoneImg, woodImg) {
-  const holder = document.createElement('div');
-  holder.classList.add('resource-area');
+  const holder = document.createElement('div')
+  holder.classList.add('resource-area')
 
   const food = resourceItem('food', foodImg)
   const stone = resourceItem('stone', stoneImg)
   const wood = resourceItem('wood', woodImg)
 
-  holder.appendChild(food);
-  holder.appendChild(stone);
-  holder.appendChild(wood);
+  holder.appendChild(food)
+  holder.appendChild(stone)
+  holder.appendChild(wood)
 
-  return holder;
+  return holder
 }
 
 function resourceItem(name, img) {
-  const resourceDiv = document.createElement('div');
-  const resourceImage = document.createElement('div');
+  const resourceDiv = document.createElement('div')
+  const resourceImage = document.createElement('div')
   resourceDiv.appendChild(resourceImage)
-  resourceImage.classList.add('resource-panel');
+  resourceImage.classList.add('resource-panel')
 
   const icon = new Image()
   icon.src = img
   icon.classList.add('resource-image')
-  resourceImage.appendChild(icon);
+  resourceImage.appendChild(icon)
 
-  const initialValue = getSavedResource(name);
-  const innerText = document.createElement('p');
-  innerText.innerHTML = initialValue;
-  innerText.classList.add('resource-text');
+  const initialValue = getSavedResource(name)
+  const innerText = document.createElement('p')
+  innerText.innerHTML = initialValue
+  innerText.classList.add('resource-text')
   resourceImage.appendChild(innerText)
 
-  const rateDiv = document.createElement('div');
-  const rateText = document.createElement('p');
-  rateText.innerHTML = '0/s';
+  const rateDiv = document.createElement('div')
+  const rateText = document.createElement('p')
+  rateText.innerHTML = '0/s'
   rateDiv.classList.add('resource-rate')
-  rateDiv.appendChild(rateText);
-  resourceDiv.appendChild(rateDiv);
+  rateDiv.appendChild(rateText)
+  resourceDiv.appendChild(rateDiv)
 
-  
   return resourceDiv
 }
 
 function getSavedResource(name) {
-  let savedResource = localStorage.getItem(name);
+  let savedResource = localStorage.getItem(name)
   if (savedResource) {
-    return savedResource;
+    return savedResource
   }
 
-  savedResource = defaultValue(name);
+  savedResource = defaultValue(name)
   return savedResource
 }
 
@@ -60,5 +59,5 @@ function defaultValue(name) {
 
   localStorage.setItem(name, 50)
 
-  return items[name];
+  return items[name]
 }
