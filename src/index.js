789cb99a28f00340285c73c5874ce3b269ad9d01
Daniel Thompson <florestina95@gmail.com>
2020-04-16 17:29:29 +0200

    importing 2 new files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash'
 import * as build from './modules/builder.js'
+import * as utils from './modules/gameCore'
 
 // initial import of images
 import woodImage from './icons/tree.png'
 import stoneImage from './icons/stone.png'
 import foodImage from './icons/food.png'
+import manImage from './icons/man.png'
 
 // initial css import
 import './css/main.css'
