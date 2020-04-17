740f3f0d12efef6adb5c481bdf36ea05b27dd200
Daniel Thompson <florestina95@gmail.com>
2020-04-17 10:12:06 +0200

    initial run function and increment test

--- src/index.js
+++ src/index.js
@@ -21,10 +21,49 @@ function resourceArea() {
   const workerPanels = build.workerPanel(manImage)
   element.appendChild(workerPanels)
 
-  return element
+  document.body.appendChild(element)
 }
 
-document.body.appendChild(component())
+function buildingsArea() {
+  const element = document.createElement('div')
+  element.setAttribute('id', 'buildingContent')
+
+  const button = document.createElement('button')
+  button.setAttribute('type', 'button')
+  button.setAttribute('id', 'farmerBuy')
+  button.innerHTML = 'buy farmer (25 food)'
+  button.addEventListener('click', () => {
+    addFarmer()
+  })
+  element.appendChild(button)
+
+  const rates = document.createElement('p')
+  rates.setAttribute('id', 'farmerRate')
+  element.appendChild(rates)
+
+  //const houses = build.housePanel()
+  //element.appendChild(houses)
+
+  document.body.appendChild(element)
+}
+
+function addFarmer() {
+  const currentFood = document.getElementById('foodValue')
+  let farmerRate = document.getElementById('farmerRate')
+  if (farmerRate.innerHTML == '') {
+    farmerRate.innerHTML = 0 + 0.1
+  } else {
+    farmerRate.innerHTML = (parseFloat(farmerRate.innerHTML) + 0.1).toFixed(2)
+  }
+  currentFood.innerHTML = (parseFloat(currentFood.innerHTML) - 25).toFixed(2)
+  const currentWorkers = document.getElementById('workerValue')
+  currentWorkers.innerHTML = parseInt(currentWorkers.innerHTML) + 1
+}
+
+function runInitial() {
+  resourceArea()
+  buildingsArea()
+}
 
 window.setInterval(() => {
   utils.progressValues()
